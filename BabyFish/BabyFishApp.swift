15adc56c1fcb6e96b9af52d34d44497086656d1b
Sunzichu <saun2003@163.com>
2023-08-07 05:00:51 +0800

    Initial Commit

---
+++ BabyFish/BabyFishApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyFishApp.swift
+//  BabyFish
+//
+//  Created by 孙子楚 on 2023/8/7.
+//
+
+import SwiftUI
+
+@main
+struct BabyFishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
